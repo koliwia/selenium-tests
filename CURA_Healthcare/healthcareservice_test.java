e1cad243257e7f17e7177f9aef49a55cc4cce195
koliwia <kloskowskaoliwia11@gmail.com>
2022-05-22 12:52:39 +0200

    change xpath

--- CURA_Healthcare/healthcareservice_test.java
+++ CURA_Healthcare/healthcareservice_test.java
@@ -25,40 +25,6 @@ public class healthcareservice_test {
                 .isDisplayed();
         Assert.assertEquals(true, make_appointment);
 
-        // Filling in the form
-        driver.findElement(By.name("facility")).sendKeys("Hongkong CURA Healthcare Center");
-
-        // Clicking on the checkbox
-        WebElement check_hospital_redmission = driver.findElement(By.id("chk_hospotal_readmission"));
-        check_hospital_redmission.click();
-
-        // Choosing the healthcare program
-        WebElement healthcare_program = driver.findElement(By.id("radio_program_medicaid"));
-        healthcare_program.click();
-
-        // Selecting a visit date
-        WebElement visit_form = driver.findElement(By.id("txt_visit_date"));
-        visit_form.click();
-
-        // Selecting a month
-        driver.findElement(By.xpath("/html/body/div/div[1]/table/thead/tr[2]/th[2]")).click();
-        driver.findElement(By.xpath("/html/body/div/div[2]/table/tbody/tr/td/span[2]")).click();
-
-        // Selecting a day
-        driver.findElement(By.xpath("/html/body/div/div[1]/table/tbody/tr[2]/td[4]")).click();
-
-        // Adding a comment
-        driver.findElement(By.id("txt_comment")).sendKeys("ASAP");
-
-        // Booking an appointment
-        WebElement bookingclick = driver.findElement(By.id("btn-book-appointment"));
-        bookingclick.click();
-
-        // Appointment Confirmation is visible
-        boolean appointment_confirmation = driver
-                .findElement(By.cssSelector("#summary > div > div > div.col-xs-12.text-center > h2"))
-                .isDisplayed();
-        Assert.assertEquals(true, appointment_confirmation);
 
         // Logging out
         driver.findElement(By.id("menu-toggle")).click();
